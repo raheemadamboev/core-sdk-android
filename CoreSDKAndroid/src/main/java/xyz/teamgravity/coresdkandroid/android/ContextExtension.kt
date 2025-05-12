b9ae7465d73e64882fdfcd2032136b71add6f29f
raheem <raheemideadev@gmail.com>
2025-05-12 23:56:00 +0500

    [1.0.17] Added catching SecurityException to start activity safely;

--- CoreSDKAndroid/src/main/java/xyz/teamgravity/coresdkandroid/android/ContextExtension.kt
+++ CoreSDKAndroid/src/main/java/xyz/teamgravity/coresdkandroid/android/ContextExtension.kt
@@ -5,12 +5,12 @@ import android.content.Context
 import android.content.Intent
 
 /**
- * Safely starts activity. If activity is not found, calls [onError] lambda.
+ * Safely starts activity. If activity is not found or not permitted, calls [onError] lambda.
  *
  * @param intent
  * Intent to start.
  * @param onError
- * Lambda that is called when starting activity is not found.
+ * Lambda that is called when starting activity is not found or not permitted.
  */
 fun Context.safelyStartActivity(
     intent: Intent,
@@ -19,6 +19,8 @@ fun Context.safelyStartActivity(
     try {
         startActivity(intent)
     } catch (e: ActivityNotFoundException) {
+        onError()
+    } catch (e: SecurityException) {
         onError()
     }
 }
