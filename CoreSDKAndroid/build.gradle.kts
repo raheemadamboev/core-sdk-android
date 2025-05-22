f4c9d855896c95b829baf52308ff7627d239bff4
raheem <raheemideadev@gmail.com>
2025-05-22 21:51:42 +0500

    [1.0.19] Fixed UpdateManager crash;

--- CoreSDKAndroid/build.gradle.kts
+++ CoreSDKAndroid/build.gradle.kts
@@ -74,7 +74,7 @@ afterEvaluate {
 
                 groupId = "com.github.raheemadamboev"
                 artifactId = "core-sdk-android"
-                version = "1.0.18"
+                version = "1.0.19"
             }
         }
     }
