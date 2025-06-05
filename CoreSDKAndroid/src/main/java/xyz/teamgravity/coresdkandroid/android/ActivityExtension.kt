32305a0189c0174d4ba3c9b9719d0ca9cdefaaa2
raheem <raheemideadev@gmail.com>
2025-06-05 23:53:00 +0500

    [1.0.24] Added overridePre34EnterTransition, overridePost34EnterTransition functions;

--- CoreSDKAndroid/src/main/java/xyz/teamgravity/coresdkandroid/android/ActivityExtension.kt
+++ CoreSDKAndroid/src/main/java/xyz/teamgravity/coresdkandroid/android/ActivityExtension.kt
@@ -2,6 +2,7 @@ package xyz.teamgravity.coresdkandroid.android
 
 import android.app.Activity
 import android.view.WindowInsets
+import androidx.annotation.AnimRes
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
@@ -35,5 +36,50 @@ fun Activity.setStatusNavigationBarColor(@ColorInt color: Int) {
         window.statusBarColor = color
         @Suppress("DEPRECATION")
         window.navigationBarColor = color
+    }
+}
+
+/**
+ * Overrides enter transition of activity on devices that run on Android 13 (API 33) or lower.
+ *
+ * @param enterAnim
+ * Enter animation resource id.
+ * @param exitAnim
+ * Exit animation resource id.
+ */
+fun Activity.overridePre34EnterTransition(
+    @AnimRes enterAnim: Int,
+    @AnimRes exitAnim: Int
+) {
+    if (!BuildUtil.atLeast34()) {
+        @Suppress("DEPRECATION")
+        overridePendingTransition(enterAnim, exitAnim)
+    }
+}
+
+/**
+ * Overrides enter transition of activity on devices that run on Android 14 (API 34) or higher.
+ *
+ * Call it in target activity's onCreate function after super.onCreate() and before setContentView().
+ *
+ * ```kotlin
+ * override fun onCreate(savedInstanceState: Bundle?) {
+ *   super.onCreate(savedInstanceState)
+ *   overridePost34EnterTransition()
+ *   setContentView()
+ * }
+ * ```
+ *
+ * @param enterAnim
+ * Enter animation resource id.
+ * @param exitAnim
+ * Exit animation resource id.
+ */
+fun Activity.overridePost34EnterTransition(
+    @AnimRes enterAnim: Int,
+    @AnimRes exitAnim: Int
+) {
+    if (BuildUtil.atLeast34()) {
+        overrideActivityTransition(Activity.OVERRIDE_TRANSITION_OPEN, enterAnim, exitAnim)
     }
 }
