c34d2e4e45ef4d3e894d5ed080af0b43c17afdd9
raheem <raheemideadev@gmail.com>
2026-04-22 01:57:17 +0500

    [1.0.44] Added new isDarkMode, isSystemInDarkMode extension functions into Context;

--- CoreSDKAndroid/src/main/java/xyz/teamgravity/coresdkandroid/android/ContextExtension.kt
+++ CoreSDKAndroid/src/main/java/xyz/teamgravity/coresdkandroid/android/ContextExtension.kt
@@ -1,8 +1,10 @@
 package xyz.teamgravity.coresdkandroid.android
 
+import android.app.UiModeManager
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import timber.log.Timber
 
 /**
@@ -26,4 +28,19 @@ fun Context.safelyStartActivity(
         Timber.e(e)
         onError()
     }
+}
+
+/**
+ * Returns `true` if the app is currently in dark mode (respects `AppCompatDelegate` overrides).
+ */
+fun Context.isDarkMode(): Boolean {
+    return (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+}
+
+/**
+ * Returns `true` if the system-wide dark mode is enabled, ignoring any app-level override.
+ */
+fun Context.isSystemInDarkMode(): Boolean {
+    val uiModeManager = getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
+    return uiModeManager.nightMode == UiModeManager.MODE_NIGHT_YES
 }
