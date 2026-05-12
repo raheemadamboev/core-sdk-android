753d28e49f94e4e4c9b06ea05e70bc19f38210ca
raheem <raheemideadev@gmail.com>
2026-05-12 21:27:18 +0500

    [1.0.46] Upgraded Gradle, AGP, dependencies to the latest version;

--- CoreSDKAndroid/build.gradle.kts
+++ CoreSDKAndroid/build.gradle.kts
@@ -70,7 +70,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.raheemadamboev"
             artifactId = "core-sdk-android"
-            version = "1.0.45"
+            version = "1.0.46"
 
             afterEvaluate {
                 from(components["release"])
